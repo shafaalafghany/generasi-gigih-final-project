068137ea316e20da68ca50e55b055118bcaf1d0a
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-20 23:59:55 +0700

    [RED] is invalid without a username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe User, type: :model do
   it 'is valid with a username and a password' do
     expect(FactoryBot.build(:user)).to be_valid
   end
+
+  it 'is invalid without a username' do
+    user = FactoryBot.build(:user, user_username: nil)
+
+    user.valid?
+
+    expect(user.errors[:user_username]).to include("can't be blank")
+  end
 end
