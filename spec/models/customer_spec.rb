56497d3e8d49038dee11c66fb8612d735d1fea33
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-21 01:40:55 +0700

    [RED] is invalid without an email

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -16,4 +16,12 @@ RSpec.describe Customer, type: :model do
 
     expect(customer.errors[:customer_name]).to include("can't be blank")
   end
+
+  it 'is invalid without an email' do
+    customer = FactoryBot.build(:customer, customer_email: nil)
+
+    customer.valid?
+
+    expect(customer.errors[:customer_email]).to include("can't be blank")
+  end
 end
