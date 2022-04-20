8e1f1d65f15dbb756c80bd55f1db4f9343bd8e17
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-21 01:36:33 +0700

    [GREEN] is valid with name, email, phone and address

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Customer, type: :model do
   it 'has a valid factory' do
     expect(FactoryBot.build(:customer)).to be_valid
   end
+
+  it 'is valid with name, email, phone and address' do
+    expect(FactoryBot.build(:customer)).to be_valid
+  end
 end
