cdc04b9dcb3d12c0c20465a85779c382c4777500
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-21 01:38:16 +0700

    [RED] is invalid without a name

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Customer, type: :model do
   it 'is valid with name, email, phone and address' do
     expect(FactoryBot.build(:customer)).to be_valid
   end
+
+  it 'is invalid without a name' do
+    customer = FactoryBot.build(:customer, customer_name: nil)
+
+    customer.valid?
+
+    expect(customer.errors[:customer_name]).to include("can't be blank")
+  end
 end
