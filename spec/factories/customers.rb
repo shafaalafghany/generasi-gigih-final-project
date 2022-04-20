3b27e84bb546c9e26b7724488db064525a6bef40
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-21 01:22:33 +0700

    add: generate model for Customer

---
+++ spec/factories/customers.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :customer do
+    customer_name { "MyString" }
+    customer_email { "MyString" }
+    customer_phone { "MyString" }
+    customer_address { "MyText" }
+  end
+end
