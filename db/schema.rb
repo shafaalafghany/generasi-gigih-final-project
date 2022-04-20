4d05fe62b6225cc0e7a0238e3df3a2ddcb2e2443
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-21 01:31:30 +0700

    add: run migrate for Customer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_20_163355) do
+ActiveRecord::Schema.define(version: 2022_04_20_181828) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "customer_name"
+    t.string "customer_email"
+    t.string "customer_phone"
+    t.text "customer_address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "user_username"
