99188043a4d2a7b1de306fdc613f27653d76abc5
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-21 17:00:58 +0700

    [GREEN] is invalid when price less than 0.0.1

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,4 +2,5 @@ class Menu < ApplicationRecord
   validates :menu_name, :menu_price, presence: true
   validates :menu_name, uniqueness: true
   validates :menu_description, length: { maximum: 150 }
+  validates :menu_price, numericality: { greater_than: 0.01 }
 end
