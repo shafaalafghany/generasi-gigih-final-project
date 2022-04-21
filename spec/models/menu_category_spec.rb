c283d6fe15eb5ed6648b1449ecfce160539d7fd0
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-21 21:46:37 +0700

    [RED] is invalid without menu_id

--- spec/models/menu_category_spec.rb
+++ spec/models/menu_category_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe MenuCategory, type: :model do
   it 'is valid with menu_id and category_id' do
     expect(FactoryBot.build(:menu_category)).to be_valid
   end
+
+  it 'is invalid without menu_id' do
+    menu_category = FactoryBot.build(:menu_category, menu_id: nil)
+
+    menu_category.valid?
+
+    expect(menu_category.errors[:menu_id]).to include("can't be blank")
+  end
 end
