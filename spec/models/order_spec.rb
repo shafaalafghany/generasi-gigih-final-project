6d492a9bb3c59315a6998a83bb68b7045a78eb13
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-22 01:35:24 +0700

    [GREEN] has a valid factory

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:order)).to be_valid
+  end
 end
