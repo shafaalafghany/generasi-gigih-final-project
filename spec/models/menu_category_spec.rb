98ad712d925724109bf014c8e76834681eb81ed2
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-21 21:41:13 +0700

    [GREEN] is valid with menu_id and category_id

--- spec/models/menu_category_spec.rb
+++ spec/models/menu_category_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe MenuCategory, type: :model do
   it 'has a valid factory' do
     expect(FactoryBot.build(:menu_category)).to be_valid
   end
+
+  it 'is valid with menu_id and category_id' do
+    expect(FactoryBot.build(:menu_category)).to be_valid
+  end
 end
