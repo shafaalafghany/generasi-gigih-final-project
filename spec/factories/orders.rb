dd861c26a5a2c1df7154ed103abf45be3eeccbc3
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-22 01:48:06 +0700

    [REFACTOR] change faker for order factory

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :order do
     order_invoice { "INV/2204220001" }
     order_total_price { 100000 }
-    order_date { "2022--4-22 08:23:01" }
+    order_date { Faker::Time.between_dates(from: Date.today - 7, to: Date.today, period: :all) }
     order_status { "NEW" }
     customer_id { 1 }
     user_id { 1 }
