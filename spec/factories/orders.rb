7592618748373b44b3aed1ee4c4ffdfee0a9387e
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-22 01:36:42 +0700

    [REFACTOR] add faker to order factory

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :order do
-    order_invoice { "MyString" }
-    order_total_price { 1 }
-    order_date { "MyText" }
+    order_invoice { "INV/2204220001" }
+    order_total_price { 100000 }
+    order_date { "2022--4-22 08:23:01" }
     order_status { "NEW" }
     customer_id { 1 }
     user_id { 1 }
