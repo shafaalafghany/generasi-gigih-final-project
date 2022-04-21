04864637ba5ace546310958e1798d0eab5afe733
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-21 15:35:08 +0700

    [GREEN] is valid with name, price and description

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Menu, type: :model do
   it 'has a valid factory' do
     expect(FactoryBot.build(:menu)).to be_valid
   end
+
+  it 'is valid with name, price and description' do
+    expect(FactoryBot.build(:menu)).to be_valid
+  end
 end
