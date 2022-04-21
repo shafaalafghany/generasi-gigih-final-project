034997b838242af087f3478806e419e7aaf03a64
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-22 02:01:06 +0700

    [GREEN] is invalid without customer_id

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,3 @@
 class Order < ApplicationRecord
-  validates :order_invoice, :order_total_price, :order_date, presence: true
+  validates :order_invoice, :order_total_price, :order_date, :customer_id, presence: true
 end
