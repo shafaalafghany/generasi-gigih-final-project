f13efdba0a11a5bf5e7033ac38effa9d4a22ddd5
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-22 01:37:25 +0700

    [GREEN] is valid with invoice, total_price, date, status

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Order, type: :model do
   it 'has a valid factory' do
     expect(FactoryBot.build(:order)).to be_valid
   end
+
+  it 'is valid with invoice, total_price, date, status' do
+    expect(FactoryBot.build(:order)).to be_valid
+  end
 end
