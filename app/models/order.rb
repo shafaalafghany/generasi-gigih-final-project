27dce3f5910458173e0d8d0eb6efec89d554ef33
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-22 02:04:53 +0700

    [REFACTOR] refactor for order factory

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,7 @@
 class Order < ApplicationRecord
-  validates :order_invoice, :order_total_price, :order_date, :customer_id, :user_id, presence: true
+  validates :order_invoice,
+  :order_total_price,
+  :order_date,
+  :customer_id,
+  :user_id, presence: true
 end
