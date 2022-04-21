ec8c522ea6986eac6fa4bd0e704ee3ed6b87d3d5
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-21 12:44:00 +0700

    [GREEN] is invalid email with an invalid email format

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
   validates :customer_name, :customer_email, presence: true
   validates :customer_email, uniqueness: true
+
+  validates_format_of :customer_email, with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 end
