f18734edfdd7a258c42fca608d68076c3396bd1f
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-22 01:48:48 +0700

    [RED] is invalid without invoice

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Order, type: :model do
   it 'is valid with invoice, total_price, date, status' do
     expect(FactoryBot.build(:order)).to be_valid
   end
+
+  it 'is invalid without invoice' do
+    order = FactoryBot.build(:order, order_invoice: nil)
+
+    order.valid?
+
+    expect(order.errors[:order_invoice]).to include("can't be blank")
+  end
 end
