c432d3f6296b0a8d52de19bac3692b1f4d08f045
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-21 21:22:00 +0700

    [RED] is invalid without a name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Category, type: :model do
   it 'is valid with a name' do
     expect(FactoryBot.build(:category)).to be_valid
   end
+
+  it 'is invalid without a name' do
+    category = FactoryBot.build(:category, category_name: nil)
+
+    category.valid?
+
+    expect(category.errors[:category_name]).to include("can't be blank")
+  end
 end
