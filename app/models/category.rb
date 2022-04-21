ed965f366cb642132981828b02bafe85355608e5
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-21 22:15:08 +0700

    [REFACTOR] add relation between categories and menu_categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  has_many :menu_categories
+  has_many :menus, through: :menu_categories
   validates :category_name, presence: true
   validates :category_name, uniqueness: true
 end
--- app/models/menu_category.rb
+++ app/models/menu_category.rb
@@ -1,5 +1,6 @@
 class MenuCategory < ApplicationRecord
   belongs_to :menu
+  belongs_to :category
 
   validates :menu_id, :category_id, presence: true
 end
