67630dce7acb555f87031674451d3c0bbae60867
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-21 15:14:23 +0700

    [GREEN] is invalid without an address

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  validates :customer_name, :customer_email, :customer_phone, presence: true
+  validates :customer_name, :customer_email, :customer_phone, :customer_address, presence: true
   validates :customer_email, uniqueness: true
 
   validates_format_of :customer_email, with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
