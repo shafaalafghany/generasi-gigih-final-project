112eb65887a6c4207c8096d999ba2bc3ced94339
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-23 06:51:47 +0700

    [GREEN] does not save the new customer in the database

--- spec/factories/menus.rb
+++ spec/factories/menus.rb
@@ -4,4 +4,10 @@ FactoryBot.define do
     menu_price { 10000 }
     menu_description { 'Some description for Menu' }
   end
+
+  factory :invalid_menu, parent: :menu do
+    menu_name { nil }
+    menu_price { nil }
+    menu_description { nil }
+  end
 end
