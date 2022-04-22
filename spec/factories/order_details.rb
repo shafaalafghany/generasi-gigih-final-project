0325f29c58235976ea178cfadcab75aa446d06b9
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-22 22:23:04 +0700

    [REFACTOR] add data faker to order_detail factory

--- spec/factories/order_details.rb
+++ spec/factories/order_details.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :order_detail do
-    menu_name { "MyString" }
-    menu_price { 1 }
+    menu_name { Faker::Food.dish }
+    menu_price { 10000 }
     menu_quantity { 1 }
     order_id { 1 }
     menu_id { 1 }
