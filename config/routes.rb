d554de8bd8cc12a638e81f767f35ba93b499443e
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-23 02:11:02 +0700

    [REFACTOR] change old routes to new routes for customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'customers/index'
-  get 'customers/show'
-  get 'customers/new'
+  resources :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
