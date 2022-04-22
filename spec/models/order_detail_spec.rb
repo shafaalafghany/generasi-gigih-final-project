b9444cbb423a4b978879c49644c2c9b7279f5742
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-22 22:36:30 +0700

    [RED] is invalid without menu_name

--- spec/models/order_detail_spec.rb
+++ spec/models/order_detail_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe OrderDetail, type: :model do
   it 'is valid with menu_name, menu_price, menu_quantity, order_id, menu_id' do
     expect(FactoryBot.build(:order_detail)).to be_valid
   end
+
+  it 'is invalid without menu_name' do
+    order_detail = FactoryBot.build(:order_detail, menu_name: nil)
+
+    order_detail.valid?
+
+    expect(order_detail.errors[:menu_name]).to include("can't be blank")
+  end
 end
