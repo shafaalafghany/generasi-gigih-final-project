7f41e2654692a710940611b4a2d11c9bee529f30
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-23 01:10:29 +0700

    refactor: add some scenarios for create method from CustomerController

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -15,4 +15,16 @@ RSpec.describe CustomersController do
     it 'assigns a new customer to @customer'
     it 'renders the :new template'
   end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new customer in the database'
+      it 'redirects to customers#show'
+    end
+    
+    context 'with invalid attributes' do
+      it 'does not save the new customer in the database'
+      it 're-renders the :new template'
+    end
+  end
 end
