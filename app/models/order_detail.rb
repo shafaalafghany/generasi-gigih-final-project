896332d8938de38f6f93eb839b52780242a57ed5
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-22 22:48:18 +0700

    [GREEN] is invalid without menu_id

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,3 +1,3 @@
 class OrderDetail < ApplicationRecord
-  validates :menu_name, :menu_price, :menu_quantity, :order_id, presence: true
+  validates :menu_name, :menu_price, :menu_quantity, :order_id, :menu_id, presence: true
 end
