d65c18e745e387bd9407304a0af4610d34be2745
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-23 01:03:53 +0700

    add: prepare scenarios for index from CustomerController

---
+++ spec/controllers/customers_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe CustomersController do
+  describe 'GET #index' do
+    it 'populates an array of all customers'
+    it 'renders the :index template'
+  end
+end
