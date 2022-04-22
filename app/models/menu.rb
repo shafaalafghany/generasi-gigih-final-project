1070f9eb931ac62167140a7658fa5329ef767f90
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-22 23:03:58 +0700

    [REFACTOR[ add relation between menus order_details

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,7 @@
 class Menu < ApplicationRecord
   has_many :menu_categories
   has_many :categories, through: :menu_categories
+  has_many :order_details
 
   validates :menu_name, :menu_price, presence: true
   validates :menu_name, uniqueness: true
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,5 +1,6 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
+  belongs_to :menu
 
   validates :menu_name, :menu_price, :menu_quantity, :order_id, :menu_id,
   presence: true
