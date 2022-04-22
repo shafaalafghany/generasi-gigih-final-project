25be5f54f62b748b5e75ebb7340dac9b100f6bea
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-23 01:05:52 +0700

    refactor: add some scenarios for show method from CustomerController

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe CustomersController do
     it 'populates an array of all customers'
     it 'renders the :index template'
   end
+
+  describe 'GET #show' do
+    it 'assigns the requested customer to @customer'
+    it  "renders the :show template"
+  end
 end
