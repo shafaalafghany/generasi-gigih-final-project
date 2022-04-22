f0b98a2dd5e3b766cdae02ee3e29d29cc666291c
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-22 22:29:42 +0700

    [GREEN] is valid with menu_name, menu_price, menu_quantity, order_id, menu_id

--- spec/models/order_detail_spec.rb
+++ spec/models/order_detail_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe OrderDetail, type: :model do
   it 'has a valid factory' do
     expect(FactoryBot.build(:order_detail)).to be_valid
   end
+
+  it 'is valid with menu_name, menu_price, menu_quantity, order_id, menu_id' do
+    expect(FactoryBot.build(:order_detail)).to be_valid
+  end
 end
