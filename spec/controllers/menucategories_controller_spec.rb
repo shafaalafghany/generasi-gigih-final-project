7c72bbcbe7ce0ec261c158949e0065e59105e6b4
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-23 08:27:42 +0700

    refactor: add some scenarios for create method

--- spec/controllers/menucategories_controller_spec.rb
+++ spec/controllers/menucategories_controller_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe MenusController do
     it "assign a new menu for @menu"
     it "renders the :new template"
   end
+
+  describe 'POST #create' do
+    context "with valid attributes" do
+      it "saves the new menu in the database"
+      it "re-renders the :new template"
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new customer in the database"
+    end
+  end
 end
