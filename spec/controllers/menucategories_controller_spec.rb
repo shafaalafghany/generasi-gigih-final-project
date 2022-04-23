33a48cecfe35c762c6b15e7bc2d9946be4e64c0a
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-23 08:33:44 +0700

    refactor: change some scenario

--- spec/controllers/menucategories_controller_spec.rb
+++ spec/controllers/menucategories_controller_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 RSpec.describe MenusController do
   before :each do
     @category = create(:category)
-    @another_category = create(:category)
     @menu = create(:menu)
   end
 
   describe 'GET #new' do
-    it "assign a new menu for @menu"
+    it "assign a new menu categories for @menu_categories"
     it "renders the :new template"
   end
 
