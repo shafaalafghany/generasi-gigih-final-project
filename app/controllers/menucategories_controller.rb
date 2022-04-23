17b1371bc4c0664bf4335cbe35e6c8caf5235685
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-23 08:06:59 +0700

    add: generate controller for MenuCategory with new method

---
+++ app/controllers/menucategories_controller.rb
@@ -0,0 +1,4 @@
+class MenucategoriesController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'menucategories/new'
   resources :menus, except: :show
   resources :customers, except: [:edit, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
