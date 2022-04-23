d0172be55a8adef9751854058998b083f5f8b1e5
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-04-23 07:58:24 +0700

    refactor: add some exception on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :menus
-  resources :customers
+  resources :menus, except: :show
+  resources :customers, except: [:edit, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
